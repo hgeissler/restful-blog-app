0b43576f72b89ed6e103ef7a7b4f401c0e6472b3
Hannes <31616109+hgeissler@users.noreply.github.com>
2020-07-04 14:41:49 +0200

    mongoose setup

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/restful_blog_app");
-
+// App config
 let app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// mongoose/model config
+mongoose.connect("mongodb://localhost/restful_blog_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// routes
 app.get("/", function (req, res) {
   res.send("Welcome");
+});
+
+// index
+app.get("/blogs", function (req, res) {
+  res.render("index");
 });
 
 app.listen(3000, function (err) {
